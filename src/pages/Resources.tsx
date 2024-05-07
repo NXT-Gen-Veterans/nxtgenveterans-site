b860ef145cb668b4ea742c30cd8f5a959068c5e3
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-05-07 19:21:03 +0100

    Organize folder structure
    - Delete unused files
    - Add pages

---
+++ src/pages/Resources.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Resources(props) {
+  return (
+    <div>Resources</div>
+  )
+}
+
+Resources.propTypes = {}
+
+export default Resources
