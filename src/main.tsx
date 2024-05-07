8f02237b0cda0b00386803196b43137d38da2f4b
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-05-07 19:19:36 +0100

    Config and setup
    - Configure typescript
    - Setup tailwind
    - Setup react-router-dom
    - Setup jest

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import Layout from "./Layout.tsx";
+import About from "pages/About.tsx";
+import Bootcamp from "./pages/Bootcamp.tsx";
+import Contact from "./pages/Contact.tsx";
+import Home from "./pages/Home.tsx";
+import Resources from "./pages/Resources.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "bootcamp",
+        element: <Bootcamp />
+      },
+      {
+        path: "resources",
+        element: <Resources />
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
