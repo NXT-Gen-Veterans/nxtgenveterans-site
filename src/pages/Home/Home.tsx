dc1c6898d5e87a2397e51c9eff3506cea3f7be8f
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-05-23 22:19:07 +0100

    Markup and style Services section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import HeroSection from "./components/HeroSection"
+import ServicesSection from "./components/ServicesSection"
 
 
 function Home() {
   return (
     <>
+      <h1 className="sr-only">Home Page</h1>
       <HeroSection />
+      <ServicesSection />
     </>
   )
 }
---
+++ src/pages/Home/components/ServicesSection.tsx
@@ -0,0 +1,78 @@
+import BookConsultation from "@/components/BookConsultation/BookConsultation";
+import Button from "@/components/Button/Button";
+
+type Service = {
+    title: string;
+    imgAlt: string;
+    btnUrl?: string;
+    btnText?: string;
+}
+
+type ServiceCardProps = {
+    service: Service;
+    index: number;
+}
+
+function ServiceCard({service, index}: ServiceCardProps) {
+    return (
+        <div className="flex flex-col items-center justify-center w-fit gap-5">
+            <div className={`flex items-center justify-center p-0.5 w-fit h-fit ${index === 1 ? "lg:rounded-[2rem]" : ""} rounded-3xl bg-gradient-to-br from-grad-blue to-ngv-blue`}>
+                <img
+                    className={` w-40 lg:w-52 ${index === 1 ? "lg:w-64 lg:rounded-[2rem]" : ""} rounded-3xl`}
+                    src={`/assets/services${index+1}.png`}
+                    alt={service.imgAlt}
+                />
+            </div>
+            <p className="font-bold lg:body-btn">{service.title}</p>
+            { 
+                service.btnUrl && service.btnText ? (
+                    <Button link={service.btnUrl} style="bg-btn-dark">
+                        {service.btnText}
+                    </Button>
+                ) : (
+                    <BookConsultation style="bg-btn-dark" />
+                )
+            }
+        </div>
+    )
+}
+
+function ServicesSection() {
+    const services: Service[] = [
+        {
+            title: "IT Bootcamp",
+            imgAlt: "",
+            btnUrl: "/bootcamp",
+            btnText: "Learn More",
+        },
+        {
+            title: "One-on-One Consultation",
+            imgAlt: "",
+        },
+        {
+            title: "Dozens of Online Resources",
+            imgAlt: "",
+            btnUrl: "/resources",
+            btnText: "Browse Now",
+        },
+    ]
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-12 px-10 py-16">
+            <h2 className="text-center">We meet you where you are</h2>
+            <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-12">
+            {
+                services.map((service, index) => (
+                    <ServiceCard
+                        key={`${index}${Math.floor(Math.random()*9)}`}
+                        service={service}
+                        index={index}
+                    />
+                ))
+            }
+            </div>
+        </div>
+    )
+}
+
+export default ServicesSection
