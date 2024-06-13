182cdd86ba9f07e386a012b40a0298538027095c
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-06-13 22:13:12 +0100

    Refactor Navigation component to use MUI
    - Refactor BookConsultation component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,50 +1,164 @@
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import { makeKey, useGlobalStore } from "store";
-
+import { makeKey, useGlobalStore, RouteType } from "store";
 import blackLogo from "assets/Logo/Black.png";
-import burgerIcon from "assets/burger-icon.svg";
-
 import BookConsultation from "components/BookConsultation/BookConsultation";
 
-function Navbar() {
-    const [isOpen, setOpen] = useState(false);
-    const isPageScrolled = useGlobalStore((state) => state.isPageScrolled);
+import MenuCloseIcon from "@mui/icons-material/ChevronRight";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import { AppBar, Backdrop, Drawer, IconButton, Link, List, ListItem, ListItemText, Tab, Tabs, Toolbar, useMediaQuery } from "@mui/material";
 
-    function handleToggleOpen() {
-        setOpen(!isOpen);
+function a11yProps(route: RouteType) {
+    return {
+        id: `${route}-tab`,
+        'aria-controls': `${route}-panel`,
+    }
+}
+
+function LogoLink() {
+    const { setNavTabValue } = useGlobalStore(state => state);
+    return (
+        <Link component={NavLink} to="/" onClick={() => {setNavTabValue("home")}}>
+            <img src={blackLogo} alt="logo" className={`w-14 lg:w-16`} />
+        </Link>
+    )
+}
+
+function Navbar() {
+    const isMobile = useMediaQuery("(max-width:1024px)");
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const {isPageScrolled, navTabValue, setNavTabValue} = useGlobalStore(state => state);
+    
+    const routes: RouteType[] = ['home', 'about', 'contact', 'resources', 'bootcamp'];
+
+    const navStyles = { 
+        backgroundColor: isPageScrolled ? "rgba(255,255,255,.5)" : "transparent",
+        boxShadow: "none",
+        backdropFilter: isPageScrolled ? "blur(20px)" : "",
+    };
+
+    function handleDrawerToggle() {
+        setDrawerOpen(!drawerOpen);
     }
 
-    function handleLogoClick() {
-        if (isOpen) handleToggleOpen();
+    function handleTabChange(event: SyntheticEvent, newValue: RouteType) {
+        event;
+        setNavTabValue(newValue);
+    }
+
+    function handleDrawerListClick(route: RouteType) {
+        setNavTabValue(route)
+        handleDrawerToggle();
     }
 
     return (
-        <nav className={`fixed top-0 left-0 z-20 w-full flex justify-between lg:justify-evenly overflow-hidden text-black lg:items-center ${isOpen ? "h-dvh lg:h-fit text-white lg:text-black bg-white/20 lg:bg-transparent backdrop-blur-[15px] lg:backdrop-blur-0" : "w-full h-fit items-center"} ${isPageScrolled ? "!bg-white/50 !backdrop-blur-[15px]" : ""}`}>
-            <NavLink to="/" onClick={handleLogoClick}>
-                <img src={blackLogo} alt="logo" className={`ml-3 lg:m-0 w-14 xl:w-16 ${isOpen && "mt-2 lg:m-0"}`} />
-            </NavLink>
-            <div className={`transition-all ease-in-out w-3/4 md:w-3/5 lg:w-fit h-full lg:h-fit flex flex-col lg:flex-row justify-start lg:justify-evenly lg:items-center gap-14 lg:gap-20 xl:gap-40 p-3 md:px-5 overflow-y-scroll lg:overflow-y-hidden ${isOpen ? "pt-3 bg-ngv-blue lg:bg-transparent" : "translate-x-full mr-16 pointer-events-none lg:translate-x-0 lg:m-0 lg:pointer-events-auto"}`}>
-                <button type="button" onClick={handleToggleOpen} className="pointer-events-auto lg:hidden">
-                    <p className="sr-only">Open or close navigation</p>
-                    <img src={burgerIcon} alt="logo" className={`transition-all ease-in-out delay-75 duration-200 w-8 ${!isOpen && "[transform:rotateY(180deg)] [filter:invert(100%)]"}`} />
-                </button>
-                <ul className={`flex flex-col lg:flex-row gap-10 lg:items-center ${!isOpen && "hidden lg:flex"}`}>
-                    {['home', 'about', 'contact', 'resources', 'bootcamp']
-                        .map(tab => (
-                            <NavLink 
-                                key={makeKey(tab)}
-                                to={tab == "home" ? "/" : `/${tab}`}
-                                className={({ isActive }) => `w-fit body-btn capitalize lg:p-2 lg:px-3 hover:bg-white/10 ${isActive && "border-l-8 lg:border-l-0 lg:border-b-4 px-5 border-white lg:border-black"}`}
-                                onClick={handleToggleOpen}
-                            >{tab}</NavLink>
-                        )
-                    )}
-                </ul>
-                <BookConsultation handleToggleOpen={handleToggleOpen} isNav={true} isNavOpen={isOpen} />
-            </div>
-        </nav>
+        isMobile ? (
+            <>
+            <AppBar position="fixed" sx={navStyles}>
+                <Toolbar sx={{ justifyContent: "space-between" }}>
+                    <LogoLink />
+                    <IconButton
+                        aria-label="open-menu"
+                        onClick={handleDrawerToggle}
+                    >
+                        <MenuOpenIcon
+                            sx={{ color: "black" }}
+                            fontSize="large"
+                        />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                anchor="right"
+                open={drawerOpen}
+                onClose={handleDrawerToggle}
+                variant="persistent"
+                PaperProps={{
+                    sx: {
+                        bgcolor: "#020281",
+                        width: "50dvw",
+                        padding: "1rem",
+                        alignItems: "start",
+                        gap: "2rem",
+                    },
+                    onClick: () => setDrawerOpen(false)
+                }}
+            >
+                <IconButton
+                    edge="start"
+                    aria-label="close-menu"
+                    onClick={handleDrawerToggle}
+                >
+                    <MenuCloseIcon
+                        color="secondary"
+                        fontSize="large" 
+                    />
+                </IconButton>
+                <List>
+                    {routes.map(route => (
+                        <ListItem
+                            key={makeKey(route)}
+                            component={NavLink}
+                            to={`/${route === 'home' ? '' : route}`}
+                            onClick={() => handleDrawerListClick(route)}
+                            { ...a11yProps(route) }
+                            sx={{
+                                borderLeft: navTabValue === route ? "5px solid white" : "",
+                                mb: "1rem",
+                            }}
+                        >
+                            <ListItemText
+                                primaryTypographyProps={{
+                                    color: "secondary",
+                                    fontWeight: "bold"
+                                }}
+                                primary={route.toUpperCase()}
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+                <div className={`${!drawerOpen ? "max-lg:hidden" : ""}`} onClick={() => {setDrawerOpen(false)}}>
+                    <BookConsultation variant />
+                </div>
+            </Drawer>
+            <Backdrop
+                open={drawerOpen}
+                onClick={handleDrawerToggle}
+                sx={{
+                    zIndex: (theme) => theme.zIndex.drawer - 1,
+                    bgcolor: "rgba(255,255,255,.5)",
+                    backdropFilter: "blur(15px)",
+                }}
+            />
+            </>
+        ) : (
+            // Tabs for larger screens
+            <AppBar position="fixed" sx={{ ...navStyles,
+                
+            }}>
+                <Toolbar sx={{ justifyContent: "space-around" }}>
+                    <LogoLink />
+                    <Tabs value={navTabValue} onChange={handleTabChange} aria-label="navigation tabs" centered>
+                        {routes.map(route => (
+                            <Tab
+                                key={makeKey(route)}
+                                component={NavLink}
+                                to={`/${route === 'home' ? '' : route}`}
+                                value={route}
+                                label={route.toUpperCase()}
+                                {...a11yProps(route)}
+                                sx={{
+                                    fontWeight: "bold",
+                                    color: "black",
+                                }}
+                            />
+                        ))}
+                    </Tabs>
+                    <BookConsultation variant />
+                </Toolbar>
+            </AppBar>
+        )
     )
 }
 
