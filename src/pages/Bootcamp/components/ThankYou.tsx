c2cb8c3ff981d9cea149659907bc08e96a5612d1
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-07-12 16:07:09 +0100

    Clean up

--- src/pages/Bootcamp/components/ThankYou.tsx
+++ src/pages/Bootcamp/components/ThankYou.tsx
@@ -17,16 +17,6 @@ function ThankYou() {
       <Link fontWeight={'bold'} href="https://www.youtube.com/@programmingwithmosh" target="_blank" rel="noopener noreferrer">https://www.youtube.com/@programmingwithmosh</Link>
     </Stack>
   )
-  return (
-    <div className="bg-ngv-blue text-white flex flex-col gap-12 items-center justify-center px-5 py-16 body-btn lg:sub-heading text-center">
-        <p className="lg:w-[min(80%,65rem)]">
-            We would like to express our gratitude to Programming with Mosh 
-            for generously providing us with some of his tutorials. 
-            For additional videos similar to these, please visit his YouTube channel.
-        </p>
-        <a className="hover:text-light-hover underline" target="_blank" href="https://www.youtube.com/@programmingwithmosh">https://www.youtube.com/@programmingwithmosh</a>
-    </div>
-  )
 }
 
 export default ThankYou
--- src/pages/Resources/components/ResourceCard.tsx
+++ src/pages/Resources/components/ResourceCard.tsx
@@ -87,30 +87,6 @@ function ResourceCard({title, link, description, bgImage}:ResourceCardProps) {
             </CardContent>
         </Card>
     )
-
-    return (
-        <div className={`bg-[${bgImage}] bg-image w-full lg:w-[min(45%,35rem)] h-56 text-center rounded-2xl`}>
-        <div className={`bg-grad-blue/80 backdrop-blur-[4.5px] w-full h-full p-5 text-center rounded-2xl overflow-hidden flex flex-col items-center justify-center gap-5 relative`}>
-            {/* Front of card */}
-            <div className={`${isFlipped ? "hidden" : "flex flex-col w-full h-full items-center justify-center"}`}>
-                <h6 className="">{title}</h6>
-                <a href={link} target="_blank" rel="noopener noreferrer" className="[word-break:break-all] text-ngv-blue hover:text-ngv-hover font-bold">{link}</a>
-            </div>
-            {/* Back of card */}
-            <div className={`${isFlipped ? "flex flex-col h-fit w-full p-5" : "hidden"} overflow-y-scroll card-scrollbar`}>
-                <p className="h-fit">{description}</p>
-                <a href={link} target="_blank" rel="noopener noreferrer" className="text-ngv-blue hover:text-ngv-hover sub-heading font-bold">Go to site</a>
-            </div>
-            <div className="h-fit absolute bottom-3 right-3 mt-5">
-                <FlipButton
-                    onClick={() => {setFlipped(state => !state)}}
-                >
-                    {isFlipped ? "Less info" : "More Info"}
-                </FlipButton>
-            </div>
-        </div>
-        </div>
-    )
 }
 
 export default ResourceCard
