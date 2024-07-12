8dbc61ad38723fc2778fe7a74a2c560333cb56ce
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-07-12 14:42:56 +0100

    Make design corrections to color paletter and H3 component

--- src/components/Headings/Headings.tsx
+++ src/components/Headings/Headings.tsx
@@ -22,7 +22,7 @@ interface H3Props {
 export function H3({ children }: H3Props) {
   const largeScreen = useGlobalStore(state => state.screen) == "lg";
   return (
-    <Typography variant={largeScreen ? "h4" : "h5"} component="h2" fontWeight="bold">
+    <Typography variant={largeScreen ? "h5" : "h6"} component="h3" fontWeight={600}>
       {children}
     </Typography>
   )
