508f82bba7ac05a472564948e41d92f0d26b0db0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-07-17 10:28:55 +0100

    Set up formsubmit

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -2,13 +2,31 @@ import { useParams } from "react-router-dom"
 import ContactForm, { ContactFormType } from "./components/ContactForm"
 import { Link, Stack, Typography } from "@mui/material";
 import { H2 } from "@/components/Headings/Headings";
+import { useEffect, useRef, useState } from "react";
 
 function Contact() {
-  let { type } = useParams();
+  const type = useRef(useParams().type);
+  const [formProps, setFormProps] = useState<{type: ContactFormType, thanks?: boolean}>({type: "General"});
 
-  type = type && type.split("").map((c,i)=>i===0?c.toUpperCase():c).join("");
+  useEffect(() => {
 
-  const formType: ContactFormType = (type === "Volunteer" || type === "Partner") ? type : "General";
+    if (type.current) type.current = type.current.split("").map((c,i)=>i===0?c.toUpperCase():c).join("");
+  
+    const formType: ContactFormType | null = 
+      (type.current === "Volunteer" || type.current === "Partner") 
+      ? type.current 
+      : (type.current === undefined)
+      ? "General"
+      : null;
+  
+    setFormProps(formType !== null ? {
+      type: formType
+    } : {
+      type: "General" as ContactFormType,
+      thanks: type.current === "Thanks" ? true : false,
+    })
+
+  }, [setFormProps])
 
   return (
     <Stack
@@ -18,7 +36,7 @@ function Contact() {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
-        height: { xs: '1000px', md: '900px' },
+        height: type.current === "Thanks" ? "500px" : { xs: '1000px', md: '900px' },
         width: '100%',
         textAlign: 'center',
         pt: { xs: 12, md: 16 },
@@ -28,8 +46,8 @@ function Contact() {
       spacing={{ xs: 6, md: 8 }}
       alignItems="center"
     >
-      <H2>We'd love to hear from you!</H2>
-      <ContactForm type={formType} />
+      <H2>{type.current === "Thanks" ? "Thanks for reaching out!" : "We'd love to hear from you!"}</H2>
+      <ContactForm {...formProps} />
       <Stack alignItems="center" justifyContent="center">
         <Stack direction={{ xs: 'column', md: 'row' }} spacing={{ md: 2 }} alignItems="center" justifyContent="center">
           <Link href="tel:+18047188630" target="_blank" rel="noopener noreferrer">+1 (804) 718 8630</Link>
